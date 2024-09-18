1c85b533adc996eec34946f5e27b6d48d46c2e72
pulgueta <retardix456@gmail.com>
2024-09-17 23:20:28 -0500

    ci: move from changeset to semantic-release

---
+++ packages/core/release.config.mjs
@@ -0,0 +1,23 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
